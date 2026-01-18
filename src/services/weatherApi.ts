319d79b67fb9420276e7365c56ed47783c27c2c9
MarianaBraga13 <maribragad@gmail.com>
2026-01-18 19:38:48 -0300

    Retoques finais

--- src/services/weatherApi.ts
+++ src/services/weatherApi.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+//Teste para ter certeza que está enviando a API KEY corretamente
+//console.log("API KEY:", import.meta.env.VITE_API_KEY);
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -8,6 +10,9 @@ const api = axios.create({
 });
 
 export const getWeather = async (city: string) => {
-  const response = await api.get(`/weather/${city}`);
+  const response = await api.get("/weather", {
+    params: { city },
+  });
+
   return response.data;
 };
